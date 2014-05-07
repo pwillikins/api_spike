d89ffca11ddaf1d04c8a9626de379ee1cff51a26
Paul Stone <pmstone247@gmail.com>
2014-05-06 22:49:49 -0600

    can search pivotal tracker stories

--- pivotal.rb
+++ pivotal.rb
@@ -5,19 +5,37 @@ Dotenv.load
 
 class Pivotal
 
-  def initialize
-    @connection = Faraday.new "https://www.pivotaltracker.com/services/v5/projects"
-    @response = @connection.get do |req|
+  def projects
+    connection = Faraday.new "https://www.pivotaltracker.com/services/v5/projects"
+    response = connection.get do |req|
       req.url "https://www.pivotaltracker.com/services/v5/projects"
       req.headers['X-TrackerToken'] = ENV['TOKEN']
     end
-  end
-
-  def projects
-    json_data = @response.body
+    json_data = response.body
     project_names = JSON.parse(json_data)
-    project_names.map {|name| name["name"]}
+    project_names.map { |name| name["name"] }
   end
 
+  def stories
+    connection = Faraday.new "https://www.pivotaltracker.com/services/v5/projects/1060096/stories?date_format=millis&with_state=unstarted"
+    response = connection.get do |req|
+      req.url "https://www.pivotaltracker.com/services/v5/projects/1060096/stories?date_format=millis&with_state=unstarted"
+      req.headers['X-TrackerToken'] = ENV['TOKEN']
+    end
+    json_data = response.body
+    project_stories = JSON.parse(json_data)
+    project_stories.map { |name| name["name"] }
+  end
+
+  def details
+    connection = Faraday.new "https://www.pivotaltracker.com/services/v5/projects/1060096/stories/69485114"
+    response = connection.get do |req|
+      req.url "https://www.pivotaltracker.com/services/v5/projects/1060096/stories/69485114"
+      req.headers['X-TrackerToken'] = ENV['TOKEN']
+    end
+    json_data = response.body
+    project_details = JSON.parse(json_data)
+    project_details["description"]
+  end
 end
 
--- spec/pivotal_spec.rb
+++ spec/pivotal_spec.rb
@@ -8,10 +8,10 @@ describe Pivotal do
     tracker = Pivotal.new
 
     expect(tracker.projects).to eq [
+                                     "Paul Stone",
                                      "Emily's URL shortener",
                                      "URLSchwartz",
                                      "gSchoolMarch2014Curriculum",
-                                     "Paul Stone",
                                      "gSchoolScreenHeroRequests",
                                      "Toolbelt - DaVinci Coders",
                                      "My Sample Project",
@@ -21,5 +21,27 @@ describe Pivotal do
                                      "Paul Stone's User Authentication",
                                      "Sparkling Hill Team CRUD"
                                    ]
+  end
+
+  it "should return a list of stories" do
+    tracker = Pivotal.new
+
+    expect(tracker.stories).to eq [
+                                    "Administrator users can see a list of all other users",
+                                    "Non administrator users are not allowed to see users list",
+                                    "Basic authorization complete",
+                                    "User cannot register if their password doesn't match the confirmation",
+                                    "User cannot register if their password is less than 3 characters",
+                                    "User cannot register if their password is blank",
+                                    "User cannot register with an email address that already exists",
+                                    "Better validation"
+                                  ]
+  end
+
+  it "should return the details of a story" do
+    tracker = Pivotal.new
+
+    expect(tracker.details).to eq "Given a logged in user\nWhen the user clicks on \"View all users\" (only visible for administrator users)\nThen they should see a list of all users, including their ids and email addresses"
+
   end
 end
